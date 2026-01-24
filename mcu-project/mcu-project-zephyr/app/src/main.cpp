2d0c90a4ac1ab92d8af27a272385a8ab15270047
Zach Warres <zwarres@gmail.com>
2026-01-23 20:18:36 -0800

    Added loopback in own thread

--- mcu-project/mcu-project-zephyr/app/src/main.cpp
+++ mcu-project/mcu-project-zephyr/app/src/main.cpp
@@ -1,9 +1,38 @@
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define COMMS_UART DT_ALIAS(comms_uart)
+
+#define COMMS_STACK_SIZE 1024
+#define COMMS_PRIORITY 7
+K_THREAD_STACK_DEFINE(comms_stack_area, COMMS_STACK_SIZE);
+struct k_thread comms_thread_data;
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+auto *external_uart = DEVICE_DT_GET(COMMS_UART);
+
+void comms_entry_point(void* p1, void* p2, void*p3)
+{
+	const struct device *uart_device = reinterpret_cast<const struct device *>(p1);
+	uint8_t rx_byte;
+
+	printk("Comms Thread Started\n");
+    while(1)
+    {
+		constexpr uint8_t tx_buf[] = {0x0, 0x1, 0x2, 0x3};
+		for(const auto& value: tx_buf)
+		{
+			uart_poll_out(external_uart, value);
+			uint8_t value_read{};
+			while(uart_poll_in(external_uart, &value_read) != 0);
+			printk("Read value %d\n", value_read);
+			k_msleep(100);
+		}
+	}
+}
 
 int main(void)
 {
@@ -16,6 +45,19 @@ int main(void)
 		return 0;
 	}
 
+	if(!device_is_ready(external_uart))
+	{
+		printk("UART device not ready\n");
+		return 0;
+	}
+
+	auto external_uart_ptr = &external_uart;
+
+	k_thread_create(&comms_thread_data, comms_stack_area,
+		K_THREAD_STACK_SIZEOF(comms_stack_area),
+		comms_entry_point,
+		(void*)(external_uart_ptr), NULL, NULL,
+		COMMS_PRIORITY, 0, K_NO_WAIT);
 
     bool led_state = false;
     while(1)
