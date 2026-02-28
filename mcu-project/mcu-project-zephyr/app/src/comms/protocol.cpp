13ff298e54625ef764005209a18bc4d475489597
Zach Warres <zwarres@gmail.com>
2026-02-27 19:27:15 -0800

    Added checksum to messaging and added test cases

--- mcu-project/mcu-project-zephyr/app/src/comms/protocol.cpp
+++ mcu-project/mcu-project-zephyr/app/src/comms/protocol.cpp
@@ -2,7 +2,7 @@
 
 #include <string.h>
 
-int protocol::process_incoming_bytes(const uint8_t* raw_bytes_in, int length_in, uint8_t* raw_bytes_out)
+int protocol::process_incoming_bytes(const uint8_t* raw_bytes_in, size_t length_in, uint8_t* raw_bytes_out)
 {
     // Must have at least header and checksum
     if(length_in < 2)
@@ -16,8 +16,8 @@ int protocol::process_incoming_bytes(const uint8_t* raw_bytes_in, int length_in,
         return 0;
     }
 
-    // TODO: Checksum check, putting newline in as placeholder
-    if(raw_bytes_in[length_in -1] != '\n')
+    const auto checksum = calculate_checksum(raw_bytes_in, length_in - 1);
+    if(raw_bytes_in[length_in -1] != checksum)
     {
         return 0;
     }
@@ -27,12 +27,23 @@ int protocol::process_incoming_bytes(const uint8_t* raw_bytes_in, int length_in,
     return processed_packet_length;
 }
 
-int protocol::build_packet(const uint8_t* raw_bytes_in, int length_in, uint8_t* packet_out)
+int protocol::build_packet(const uint8_t* raw_bytes_in, size_t length_in, uint8_t* packet_out)
 {
     int final_packet_size = length_in + 2;
     packet_out[0] = 0xAA;
     memcpy(&packet_out[1], raw_bytes_in, length_in);
-    // TODO: should be a checksum
-    packet_out[final_packet_size - 1] = '\n';
+
+    const auto checksum = calculate_checksum(packet_out, final_packet_size - 1);
+    packet_out[final_packet_size - 1] = checksum;
     return final_packet_size;
+}
+
+uint8_t protocol::calculate_checksum(const uint8_t* payload, size_t length)
+{
+    uint8_t checksum = 0;
+    for(size_t i = 0; i < length; i++)
+    {
+        checksum ^= payload[i];
+    }
+    return checksum;
 }
--- mcu-project/mcu-project-zephyr/app/src/comms/protocol.hpp
+++ mcu-project/mcu-project-zephyr/app/src/comms/protocol.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <stddef.h>
 
+// TODO: This does not have any error recovery yet if a byte is missed
 namespace protocol
 {
     /// Looks for header and checksum of incoming packet, if correct header and checksum are
     /// present then strips header and checksum into raw_bytes_out and returns final packet length.
     /// Returns 0 if packet failed to process.
-    int process_incoming_bytes(const uint8_t* raw_bytes_in, int length_in, uint8_t* raw_bytes_out);
+    /// TODO: figure out way to discard data if checksum fails (need fixed packet sizes)
+    int process_incoming_bytes(const uint8_t* raw_bytes_in, size_t length_in, uint8_t* raw_bytes_out);
 
     /// Adds header and checksum to a packet and copies that data into packet_out. Returns final
     /// packet size.
-    int build_packet(const uint8_t* raw_bytes_in, int length_in, uint8_t* packet_out);
+    int build_packet(const uint8_t* raw_bytes_in, size_t length_in, uint8_t* packet_out);
+
+    /// Calculates a checksum by XORing each successive byte and returns the calculated checksum
+    uint8_t calculate_checksum(const uint8_t* payload, size_t length);
 }
